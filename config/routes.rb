3f0614734a40fc7142ac4408bfc23227a27262f9
eilianacar <eilianacar@gmail.com>
2023-06-12 19:08:01 -0400

    agregando el navbar y haciendo cambios en css personalizado

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/projects'
-  get 'pages/contact'
+  root "pages#home"
+  get '/projects', to:"pages#projects"
+  get '/contact', to:"pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
